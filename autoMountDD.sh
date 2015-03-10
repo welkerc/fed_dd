915f55ad9cf5d6f2c2677fb003beb1517dda5c21
Christopher <paradigms@gmail.com>
2015-03-09 17:25:47 -0700

    added alot

--- autoMountDD.sh
+++ autoMountDD.sh
@@ -3,8 +3,25 @@
 #the dd command to clone paritions automounted in the /run/media/liveuser/ to
 #a defined network share.
 
+#setting the time stamp that will be the name of the destination folder
+timestamp=$(date +%m%d%y_%H%m%S)
+
+#setting the name of the home folder
+home=cwelker
+
 #find out what has been mounted
-srcDev="$(mount | grep "cwelker")"
+srcDev="$(mount | grep "$home")"
+hdd="${srcDev:0:9}"
 
 #unmounting the partition(s)
-umount ${srcDev:0:9}
+umount $hdd
+
+#create the folder (for now locally in the homefolder)
+mkdir -p /home/$home/$timestamp
+
+#create a file that can be used as the file location later. 
+path=/home/$home/$timestamp/$timestamp.iso
+touch $path
+
+#running dd to clone the drive
+dd if=$hdd of=$path
